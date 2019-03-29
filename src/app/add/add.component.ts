dd1c149bb0d570b26047a09d8c03c60d1cb5a58e
phucngovan <phucdepzai0712@gmail.com>
2019-03-29 11:05:12 +0700

    formbuider<=>formcontroler

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -9,25 +9,40 @@ import {EmployeeService} from '../employee.service';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
+  addForm: FormGroup;
+  submitted = false;
 
   constructor(private formBuilder: FormBuilder, private router: Router, private employeeService: EmployeeService) { }
-  addForm = new FormGroup({
-    id: new FormControl(''),
-    name: new FormControl('', [
-      Validators.required,
-      Validators.minLength(4)
-    ]),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.pattern('(\\W|^)[\\w.+\\-]*@gmail\\.com(\\W|$)')
-    ]),
-    birthday: new FormControl(''),
-    gender: new FormControl('')
-  });
+  // addForm = new FormGroup({
+  //   id: new FormControl(''),
+  //   name: new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(4)
+  //   ]),
+  //   email: new FormControl('', [
+  //     Validators.required,
+  //     Validators.pattern('(\\W|^)[\\w.+\\-]*@gmail\\.com(\\W|$)')
+  //   ]),
+  //   birthday: new FormControl(''),
+  //   gender: new FormControl('')
+  // });
+  get f() { return this.addForm.controls; }
 
   ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      name: ['',[Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.pattern('^([\\w]*[\\w\\.]*(?!\\.)@gmail.com)')]],
+      birthday: ['', Validators.required],
+      gender: ['', Validators.required]
+    })
   }
   onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.addForm.invalid) {
+      return;
+    }
       this.employeeService.addEmployee(this.addForm.value)
         .subscribe( data => {
           this.router.navigate(['list-employee']).then(function() {
